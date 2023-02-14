b99ec80343030befdd3ffac9d9db96bb6fac4799
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-02-15 00:49:12 +0300

    added a handler for register page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"go-concurrency/data"
+	"html/template"
 	"net/http"
 )
 
@@ -71,12 +74,46 @@ func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	// TODO - Validate data (kullanıcının girdiği veriler doğru mu, kullanıcı daha önceden kayıtlı mı vs.)
+
 	// create a user
+	u := data.User{
+		Email:     r.Form.Get("email"),
+		FirstName: r.Form.Get("first-name"),
+		LastName:  r.Form.Get("last-name"),
+		Password:  r.Form.Get("password"),
+		Active:    0,
+		IsAdmin:   0,
+	}
 
-	// send an activation email (processing time & delay açısından expensive olabilir o yüzden concurrency ile arkaplanda halledeceğiz.)
-	// email ile gönderdiğimiz link tıklandığında aşağıdaki fonksiyonu çalıştıracak.
+	_, err = u.Insert(u)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Unable to create user.")
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+		return
+	}
 
-	// subscribe the user to an account
+	// send an activation email
+	// normalde env.var.'dan almamız lazım burada basitçe hardcoded şekilde yazdık.
+	url := fmt.Sprintf("http://localhost/activate?email=%s", u.Email)
+	signedURL := GenerateTokenFromString(url) // prevents url tampering
+	app.InfoLog.Println(signedURL)            // just printed it to see
+
+	msg := Message{
+		To:       u.Email,
+		Subject:  "Activate your account",
+		Template: "confirmation-email",
+		Data:     template.HTML(signedURL), // any olduğu için herhangi bir tipte veri koyabildik.
+	}
+
+	app.sendEmail(msg)
+	app.Session.Put(r.Context(), "flash", "Confirmation email sent. Check your email.")
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
