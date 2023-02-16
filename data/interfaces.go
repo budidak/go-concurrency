9b9422b9361de544a06520d57d743d7b22e81ede
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-02-17 02:35:59 +0300

    modifying the data package to make it testable

---
+++ data/interfaces.go
@@ -0,0 +1,30 @@
+package data
+
+// Normalde main.go içinde application config parametrelerini verirken Models: data.New(db) yazmıştık.
+// Bu test aşamasında isteyeceğimiz bir şey değil çünkü database'e bağlanmaya çalışacak.
+// Yani testleri çalıştırırken, arkaplanda çalışan bir database isteyecek.
+// Çünkü models.go içinde New() fonksiyonuna baktığımızda
+// database ile iletişimde olan iki tane struct oluşturup onları Models olarak dönecek.
+// Bunu istemiyoruz, database ile doğrudan konuşmasın diye aynı metotları şart koşan interfaces yazdık.
+// Artık yeni haliyle, testleri çalıştırdığımızda New() çağrıldığında database ile doğrudan konuşmayan yapılar var elimizde.
+
+// User type satisfies this interface because it has all these methods
+type UserInterface interface {
+	GetAll() ([]*User, error)
+	GetByEmail(email string) (*User, error)
+	GetOne(id int) (*User, error)
+	Update() error
+	Delete() error
+	DeleteByID(id int) error
+	Insert(user User) (int, error)
+	ResetPassword(password string) error
+	PasswordMatches(plainText string) (bool, error)
+}
+
+// Model type satisfies this interface because it has all these methods
+type PlanInterface interface {
+	GetAll() ([]*Plan, error)
+	GetOne(id int) (*Plan, error)
+	SubscribeUserToPlan(user User, plan Plan) error
+	AmountForDisplay() string
+}
